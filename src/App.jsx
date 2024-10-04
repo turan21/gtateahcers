75178707eed616b1fa13699debf2b40b1176ece8
Turan Kutuev <turan.kutuev@onoisoftware.com>
2024-10-04 22:53:57 +0600

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,129 @@
-import "./App.css"
+import "./App.css";
 import React, { useState } from "react"; // Added useState
 import GameScreen from "./GameScree";
 
 function App() {
   let characters = [
     { name: "Ibragim Gubarov", image: "ibo.png", bg: "ibo.webp", title: "ICT" },
-    { name: "Muhammad Usama", image: "usama.png", bg: "muha.jpg", title: "Biology" },
-    { name: "Kumar Imanalieva", image: "kumar.png", bg: "7.jpg", title: "Kyrgyz Language" },
-    { name: "Tolubai Nadyrov", image: "tolubai.png", bg: "tolik.webp", title: "Math" },
-    { name: "Temirlan Erkinbekov", image: "tombom.png", bg: "tempo.webp", title: "Chemistry" },
-    { name: "Nargiza Isakeeva", image: "nargiza.png", bg: "nargiza.webp", title: "Kyrgyz Language" },
-    { name: "Chyngyz Zhumaliev", image: "chyngyz.png", bg: "chyngyz.webp", title: "Military" },
-    { name: "Baktygul Zhamangulova", image: "baku.png", bg: "baku.webp", title: "Geography" },
-    { name: "Atabek Zhakeev", image: "atabek.png", bg: "atabek.jpg", title: "Math" },
-    { name: "Kiyaz Amangulov", image: "kyiaz.png", bg: "coco.webp", title: "Technology" },
-    { name: "Aibek Temirkanov", image: "aibo.png", bg: "8.jpg", title: "Physical Education" },
-    { name: "Daniar Bazarkulov", image: "doni.png", bg: "2.jpg", title: "Physical Education" },
+    {
+      name: "Muhammad Usama",
+      image: "usama.png",
+      bg: "muha.jpg",
+      title: "Biology",
+    },
+    {
+      name: "Kumar Imanalieva",
+      image: "kumar.png",
+      bg: "7.jpg",
+      title: "Kyrgyz Language",
+    },
+    {
+      name: "Tolubai Nadyrov",
+      image: "tolubai.png",
+      bg: "tolik.webp",
+      title: "Math",
+    },
+    {
+      name: "Temirlan Erkinbekov",
+      image: "tombom.png",
+      bg: "tempo.webp",
+      title: "Chemistry",
+    },
+    {
+      name: "Nargiza Isakeeva",
+      image: "nargiza.png",
+      bg: "nargiza.webp",
+      title: "Kyrgyz Language",
+    },
+    {
+      name: "Chyngyz Zhumaliev",
+      image: "chyngyz.png",
+      bg: "chyngyz.webp",
+      title: "Military",
+    },
+    {
+      name: "Baktygul Zhamangulova",
+      image: "baku.png",
+      bg: "bakubg.png",
+      title: "Geography",
+    },
+    {
+      name: "Bekbolsun Ilias uulu",
+      image: "beka.png",
+      bg: "gym.png",
+      title: "Dormitory director",
+    },
+    {
+      name: "Atabek Zhakeev",
+      image: "atabek.png",
+      bg: "atabek.jpg",
+      title: "Math",
+    },
+    {
+      name: "Kiyaz Amangulov",
+      image: "kyiaz.png",
+      bg: "coco.webp",
+      title: "Technology",
+    },
+    {
+      name: "Aibek Temirkanov",
+      image: "aibo.png",
+      bg: "8.jpg",
+      title: "Physical Education",
+    },
+    {
+      name: "Daniar Bazarkulov",
+      image: "doni.png",
+      bg: "2.jpg",
+      title: "Physical Education",
+    },
 
+    {
+      name: "Ulan Boshkoev",
+      image: "ulan.png",
+      bg: "ulanbg.png",
+      title: "English, Economics",
+    },
+    {
+      name: "Baktygul Sultanova",
+      image: "bakyt.png",
+      bg: "bakytbg.png",
+      title: "Russian Language",
+    },
+    {
+      name: "Tilenbubu Sultanalieva",
+      image: "tilek.png",
+      bg: "tillekbg.png",
+      title: "Library",
+    },
+    {
+      name: "Zhumabek Ushurov",
+      image: "zhuma.png",
+      bg: "zhumabg.png",
+      title: "Math",
+    },
+    {
+      name: "Zhunus Akimov",
+      image: "zhunus.png",
+      bg: "zhunusbg.png",
+      title: "Accountant",
+    },
+    {
+      name: "Erlan Satarov",
+      image: "erra.png",
+      bg: "erabg.png",
+      title: "Principal",
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0); // Added state for current character
-  const [showName, setShowName] = useState(false)
+  const [showName, setShowName] = useState(false);
   const nextCharacter = () => {
     if (!showName) {
-      setShowName(true)
+      setShowName(true);
     } else {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % characters.length); // Function to change character
-      setShowName(false)
+      setShowName(false);
     }
   };
 
@@ -38,7 +135,10 @@ function App() {
         characterImage={characters[currentIndex].image}
         bg={characters[currentIndex].bg}
       />
-      <button className="btn" onClick={nextCharacter}>Next Character</button> {/* Button to change character */}
+      <button className="btn" onClick={nextCharacter}>
+        Next Character
+      </button>{" "}
+      {/* Button to change character */}
     </div>
   );
 }
